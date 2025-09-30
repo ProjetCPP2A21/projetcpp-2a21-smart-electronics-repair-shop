8de32d6d62bb9b4bea105e98db51f34f9d9d5fdb
adem-gharbi <gharbi.adem@esprit.tn>
2025-09-30 13:09:46 +0100

    Add files via upload

---
+++ Gemployer.pro
@@ -0,0 +1,13 @@
+QT       += core gui widgets
+
+TARGET = Gemployer
+TEMPLATE = app
+
+SOURCES += main.cpp \
+           mainwindow.cpp
+
+HEADERS += mainwindow.h
+
+FORMS   += mainwindow.ui
+
+RESOURCES +=
--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
-#include <QApplication>
 #include "mainwindow.h"
+#include <QApplication>
+#include <QFile>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
+
+    // Charger style.css
+    QFile styleFile(":/style.qss");
+    if (styleFile.open(QFile::ReadOnly)) {
+        QString style = styleFile.readAll();
+        a.setStyleSheet(style);
+    }
+
     MainWindow w;
     w.show();
     return a.exec();
