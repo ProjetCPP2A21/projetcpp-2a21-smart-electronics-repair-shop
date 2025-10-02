1d460961a8b86f9bf04010e27b641cbd2eb40183
nourhene <nourhene.bjaoui@esprit.tn>
2025-10-02 21:09:16 +0100

    Ajout du projet Qt

---
+++ build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,289 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 6.7.3
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDateEdit>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QLineEdit *lineEdit_11;
+    QLineEdit *lineEdit_12;
+    QLineEdit *lineEdit_13;
+    QLineEdit *lineEdit_14;
+    QLineEdit *lineEdit_15;
+    QLineEdit *lineEdit_16;
+    QLabel *label_2;
+    QLabel *label_3;
+    QLabel *label_4;
+    QLabel *label_5;
+    QLabel *label_6;
+    QLabel *label_7;
+    QLineEdit *lineEdit_17;
+    QLineEdit *lineEdit_18;
+    QLabel *label_8;
+    QLabel *label_9;
+    QLineEdit *lineEdit_19;
+    QLabel *label_10;
+    QLineEdit *lineEdit_20;
+    QLabel *label_11;
+    QDateEdit *dateEdit_3;
+    QTableWidget *tableWidget;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
+    QPushButton *pushButton_5;
+    QLineEdit *lineEdit;
+    QLabel *label;
+    QLabel *label_12;
+    QPushButton *pushButton_6;
+    QPushButton *pushButton_7;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName("MainWindow");
+        MainWindow->resize(1544, 717);
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color: rgb(0, 0, 127);"));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        lineEdit_11 = new QLineEdit(centralwidget);
+        lineEdit_11->setObjectName("lineEdit_11");
+        lineEdit_11->setGeometry(QRect(160, 100, 113, 26));
+        lineEdit_11->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_12 = new QLineEdit(centralwidget);
+        lineEdit_12->setObjectName("lineEdit_12");
+        lineEdit_12->setGeometry(QRect(160, 180, 113, 26));
+        lineEdit_12->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_13 = new QLineEdit(centralwidget);
+        lineEdit_13->setObjectName("lineEdit_13");
+        lineEdit_13->setGeometry(QRect(160, 140, 113, 26));
+        lineEdit_13->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_14 = new QLineEdit(centralwidget);
+        lineEdit_14->setObjectName("lineEdit_14");
+        lineEdit_14->setGeometry(QRect(160, 220, 113, 26));
+        lineEdit_14->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_15 = new QLineEdit(centralwidget);
+        lineEdit_15->setObjectName("lineEdit_15");
+        lineEdit_15->setGeometry(QRect(160, 260, 113, 26));
+        lineEdit_15->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_16 = new QLineEdit(centralwidget);
+        lineEdit_16->setObjectName("lineEdit_16");
+        lineEdit_16->setGeometry(QRect(160, 300, 113, 26));
+        lineEdit_16->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName("label_2");
+        label_2->setGeometry(QRect(30, 100, 71, 20));
+        label_2->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_3 = new QLabel(centralwidget);
+        label_3->setObjectName("label_3");
+        label_3->setGeometry(QRect(30, 140, 71, 20));
+        label_3->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_4 = new QLabel(centralwidget);
+        label_4->setObjectName("label_4");
+        label_4->setGeometry(QRect(30, 180, 71, 20));
+        label_4->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_5 = new QLabel(centralwidget);
+        label_5->setObjectName("label_5");
+        label_5->setGeometry(QRect(30, 220, 71, 20));
+        label_5->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_6 = new QLabel(centralwidget);
+        label_6->setObjectName("label_6");
+        label_6->setGeometry(QRect(30, 260, 71, 20));
+        label_6->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_7 = new QLabel(centralwidget);
+        label_7->setObjectName("label_7");
+        label_7->setGeometry(QRect(30, 300, 71, 20));
+        label_7->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        lineEdit_17 = new QLineEdit(centralwidget);
+        lineEdit_17->setObjectName("lineEdit_17");
+        lineEdit_17->setGeometry(QRect(160, 340, 113, 26));
+        lineEdit_17->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        lineEdit_18 = new QLineEdit(centralwidget);
+        lineEdit_18->setObjectName("lineEdit_18");
+        lineEdit_18->setGeometry(QRect(160, 380, 113, 26));
+        lineEdit_18->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        label_8 = new QLabel(centralwidget);
+        label_8->setObjectName("label_8");
+        label_8->setGeometry(QRect(30, 340, 91, 20));
+        label_8->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_9 = new QLabel(centralwidget);
+        label_9->setObjectName("label_9");
+        label_9->setGeometry(QRect(30, 380, 101, 20));
+        label_9->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        lineEdit_19 = new QLineEdit(centralwidget);
+        lineEdit_19->setObjectName("lineEdit_19");
+        lineEdit_19->setGeometry(QRect(160, 420, 113, 26));
+        lineEdit_19->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        label_10 = new QLabel(centralwidget);
+        label_10->setObjectName("label_10");
+        label_10->setGeometry(QRect(30, 420, 101, 20));
+        label_10->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        lineEdit_20 = new QLineEdit(centralwidget);
+        lineEdit_20->setObjectName("lineEdit_20");
+        lineEdit_20->setGeometry(QRect(160, 460, 113, 26));
+        lineEdit_20->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);"));
+        label_11 = new QLabel(centralwidget);
+        label_11->setObjectName("label_11");
+        label_11->setGeometry(QRect(30, 460, 111, 20));
+        label_11->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        dateEdit_3 = new QDateEdit(centralwidget);
+        dateEdit_3->setObjectName("dateEdit_3");
+        dateEdit_3->setGeometry(QRect(160, 460, 110, 26));
+        tableWidget = new QTableWidget(centralwidget);
+        if (tableWidget->columnCount() < 9)
+            tableWidget->setColumnCount(9);
+        QTableWidgetItem *__qtablewidgetitem = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(0, __qtablewidgetitem);
+        QTableWidgetItem *__qtablewidgetitem1 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(1, __qtablewidgetitem1);
+        QTableWidgetItem *__qtablewidgetitem2 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(2, __qtablewidgetitem2);
+        QTableWidgetItem *__qtablewidgetitem3 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(3, __qtablewidgetitem3);
+        QTableWidgetItem *__qtablewidgetitem4 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(4, __qtablewidgetitem4);
+        QTableWidgetItem *__qtablewidgetitem5 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(5, __qtablewidgetitem5);
+        QTableWidgetItem *__qtablewidgetitem6 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(6, __qtablewidgetitem6);
+        QTableWidgetItem *__qtablewidgetitem7 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(7, __qtablewidgetitem7);
+        QTableWidgetItem *__qtablewidgetitem8 = new QTableWidgetItem();
+        tableWidget->setHorizontalHeaderItem(8, __qtablewidgetitem8);
+        if (tableWidget->rowCount() < 8)
+            tableWidget->setRowCount(8);
+        tableWidget->setObjectName("tableWidget");
+        tableWidget->setGeometry(QRect(410, 230, 1081, 331));
+        tableWidget->setRowCount(8);
+        tableWidget->setColumnCount(9);
+        tableWidget->horizontalHeader()->setDefaultSectionSize(120);
+        tableWidget->verticalHeader()->setVisible(false);
+        tableWidget->verticalHeader()->setProperty("showSortIndicator", QVariant(false));
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName("pushButton");
+        pushButton->setGeometry(QRect(440, 590, 93, 29));
+        pushButton->setStyleSheet(QString::fromUtf8("background-color: rgb(18, 153, 45);"));
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName("pushButton_2");
+        pushButton_2->setGeometry(QRect(1320, 180, 93, 29));
+        pushButton_2->setStyleSheet(QString::fromUtf8("background-color: rgb(253, 7, 27);"));
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(1210, 180, 93, 29));
+        pushButton_3->setStyleSheet(QString::fromUtf8("background-color: rgb(253, 145, 4);"));
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName("pushButton_4");
+        pushButton_4->setGeometry(QRect(60, 520, 93, 29));
+        pushButton_4->setStyleSheet(QString::fromUtf8("background-color: rgb(18, 153, 45);"));
+        pushButton_5 = new QPushButton(centralwidget);
+        pushButton_5->setObjectName("pushButton_5");
+        pushButton_5->setGeometry(QRect(420, 180, 93, 29));
+        pushButton_5->setStyleSheet(QString::fromUtf8("background-color: rgb(203, 203, 203);"));
+        lineEdit = new QLineEdit(centralwidget);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(560, 180, 221, 26));
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(80, 40, 141, 20));
+        label->setStyleSheet(QString::fromUtf8("color: rgb(255, 255, 255);"));
+        label_12 = new QLabel(centralwidget);
+        label_12->setObjectName("label_12");
+        label_12->setGeometry(QRect(1150, 20, 121, 111));
+        pushButton_6 = new QPushButton(centralwidget);
+        pushButton_6->setObjectName("pushButton_6");
+        pushButton_6->setGeometry(QRect(170, 520, 93, 29));
+        pushButton_6->setStyleSheet(QString::fromUtf8("background-color: rgb(253, 7, 27);"));
+        pushButton_7 = new QPushButton(centralwidget);
+        pushButton_7->setObjectName("pushButton_7");
+        pushButton_7->setGeometry(QRect(1360, 590, 101, 29));
+        pushButton_7->setStyleSheet(QString::fromUtf8("background-color: rgb(203, 203, 203);"));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName("menubar");
+        menubar->setGeometry(QRect(0, 0, 1544, 26));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName("statusbar");
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label_2->setText(QCoreApplication::translate("MainWindow", "ID Pi\303\250ce", nullptr));
+        label_3->setText(QCoreApplication::translate("MainWindow", "Nom", nullptr));
+        label_4->setText(QCoreApplication::translate("MainWindow", "ID Pi\303\250ce", nullptr));
+        label_5->setText(QCoreApplication::translate("MainWindow", "R\303\250f\303\250rence", nullptr));
+        label_6->setText(QCoreApplication::translate("MainWindow", "Cat\303\251gorie ", nullptr));
+        label_7->setText(QCoreApplication::translate("MainWindow", "Quantit\303\251", nullptr));
+        label_8->setText(QCoreApplication::translate("MainWindow", "Prix d'achat", nullptr));
+        label_9->setText(QCoreApplication::translate("MainWindow", "Prix de vente", nullptr));
+        label_10->setText(QCoreApplication::translate("MainWindow", "Fournisseur", nullptr));
+        label_11->setText(QCoreApplication::translate("MainWindow", "Date de livraison", nullptr));
+        QTableWidgetItem *___qtablewidgetitem = tableWidget->horizontalHeaderItem(0);
+        ___qtablewidgetitem->setText(QCoreApplication::translate("MainWindow", "ID Pi\303\250ce", nullptr));
+        QTableWidgetItem *___qtablewidgetitem1 = tableWidget->horizontalHeaderItem(1);
+        ___qtablewidgetitem1->setText(QCoreApplication::translate("MainWindow", "Nom", nullptr));
+        QTableWidgetItem *___qtablewidgetitem2 = tableWidget->horizontalHeaderItem(2);
+        ___qtablewidgetitem2->setText(QCoreApplication::translate("MainWindow", "R\303\251f\303\251rence", nullptr));
+        QTableWidgetItem *___qtablewidgetitem3 = tableWidget->horizontalHeaderItem(3);
+        ___qtablewidgetitem3->setText(QCoreApplication::translate("MainWindow", "Cat\303\251gorie", nullptr));
+        QTableWidgetItem *___qtablewidgetitem4 = tableWidget->horizontalHeaderItem(4);
+        ___qtablewidgetitem4->setText(QCoreApplication::translate("MainWindow", "Quanti\303\251 ", nullptr));
+        QTableWidgetItem *___qtablewidgetitem5 = tableWidget->horizontalHeaderItem(5);
+        ___qtablewidgetitem5->setText(QCoreApplication::translate("MainWindow", "Prix d'achat", nullptr));
+        QTableWidgetItem *___qtablewidgetitem6 = tableWidget->horizontalHeaderItem(6);
+        ___qtablewidgetitem6->setText(QCoreApplication::translate("MainWindow", "prix de vente", nullptr));
+        QTableWidgetItem *___qtablewidgetitem7 = tableWidget->horizontalHeaderItem(7);
+        ___qtablewidgetitem7->setText(QCoreApplication::translate("MainWindow", "Fournisseur", nullptr));
+        QTableWidgetItem *___qtablewidgetitem8 = tableWidget->horizontalHeaderItem(8);
+        ___qtablewidgetitem8->setText(QCoreApplication::translate("MainWindow", "Date de livraison", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Mise \303\240 jour", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Supprimer", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Modifier", nullptr));
+        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Ajouter", nullptr));
+        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Rechercher", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Ajouter une pi\303\251ce", nullptr));
+        label_12->setText(QString());
+        pushButton_6->setText(QCoreApplication::translate("MainWindow", "Supprimer", nullptr));
+        pushButton_7->setText(QCoreApplication::translate("MainWindow", "Exporter PDF", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
