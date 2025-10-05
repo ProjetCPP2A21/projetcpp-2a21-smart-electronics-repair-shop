9e44f8c8913b0fa46d415c341d8cd7807d7c5e73
sarahbenguiza <sarah.benguiza@esprit.tn>
2025-10-05 16:37:07 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
-#include <QApplication>
-#include "mainwindow.h"
+#include "client.h"
 
-int main(int argc, char *argv[]) {
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "sans_titre2_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
     MainWindow w;
     w.show();
     return a.exec();
