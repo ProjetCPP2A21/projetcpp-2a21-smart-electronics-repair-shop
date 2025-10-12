9d60d1642f0e12101dc11bfb016c10b3cc5d02ff
ReonXI <dridi.ahmed.2@esprit.tn>
2025-10-12 13:48:37 +0100

    removed unwanted files

--- fournisseurmain.cpp
+++
@@ -1,9 +0,0 @@
-#include "fournisseurwidget.h"
-#include <QApplication>
-
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    FournisseurWidget w;
-    w.show();
-    return app.exec();
-}
