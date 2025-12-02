f559724a41b3d34dacff96aac1f076634f237e0a
ReonXI <dridi.ahmed.2@esprit.tn>
2025-12-02 08:48:44 +0100

    integration appareils, stock, employe, fournisseur

---
+++ integration_metiers_a_s_e_f/connection.cpp
@@ -0,0 +1,46 @@
+#include "connection.h"
+
+// Constructeur: s'exécute une seule fois
+Connection::Connection()
+{
+    // On ajoute la base de données en utilisant le pilote ODBC
+    db = QSqlDatabase::addDatabase("QODBC");
+}
+
+// Destructeur: ferme la connexion si elle est ouverte
+Connection::~Connection()
+{
+    if (db.isOpen())
+        db.close();
+}
+
+// Méthode 'instance' (Singleton)
+Connection& Connection::instance()
+{
+    static Connection instance; // L'unique instance est créée ici
+    return instance;
+}
+
+// Méthode pour établir la connexion
+bool Connection::createConnection()
+{
+    // On définit les paramètres de connexion
+    // 1. Le nom du DSN ODBC (configuré dans Windows)
+     db.setDatabaseName("electronicshop");
+
+    // 2. L'utilisateur (schéma)
+    db.setUserName("electronicshop");
+
+    // 3. Le mot de passe
+    db.setPassword("0101");
+
+    // On tente d'ouvrir la connexion
+    if (db.open()) {
+        qDebug() << "Connexion établie ✅";
+        return true;
+    } else {
+        // En cas d'échec, on affiche l'erreur
+        qDebug() << "Échec de la connexion ❌:" << db.lastError().text();
+        return false;
+    }
+}
